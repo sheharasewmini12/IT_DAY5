a04eb1326c3b64a207885e8712df83b43b8c7c23
sheharasewmini12 <shehara.sewmini.ul@gmail.com>
2026-07-10 09:35:07 +0530

    Comment out unused variables and logging statements in app.js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,27 @@
- let name = "Saman";
+//  let name = "Saman";
 
- let age = 18;
+//  let age = 18;
 
- name = "ranil";
+//  name = "ranil";
 
- console.log(name);
- console.log(age);
+//  console.log(name);
+//  console.log(age);
 
- alert("Hello icet");
+//  alert("Hello icet");
+
+// let name = "saman";
+// let age = 18;
+// let isAfterAL = false;
+// let customers = [];
+// let customer = {};
+// let salary = 7500.0;
+
+
+// console.log(typeof name);
+
+let x = 10;
+let y = 20;
+
+let z = x * y;
+
+console.log(z);
